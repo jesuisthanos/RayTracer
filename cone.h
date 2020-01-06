0ca40fd46a810360269e8ad5774946236aff803e
Hồ Sỹ Thành <hosythanh14@gmail.com>
2020-01-06 16:16:26 +0100

    added cone

---
+++ cone.h
@@ -0,0 +1,32 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
+#include "object.h"
+
+class Cone : public Object
+{
+public:
+	Point base;
+	Point top;
+	double radius;
+	Cone(Point v1, Point v2, Point v3, bool rightSide) : vertex1(v1), vertex2(v2), vertex3(v3), rightSide(rightSide) {
+		Vector norm = (vertex1 - vertex2).cross(vertex1 - vertex3);
+		normal = norm / norm.length();
+	}
+	
+	Vector getNormal();
+	void setNormal(Vector newNorm);
+
+	virtual Hit intersect(const Ray& ray);
+    virtual double getZPos();
+    virtual Triple mapTexture(const Ray &ray, const Hit &hit, const Point &point);
+	virtual string objType();
+
+	const Point vertex1;
+	const Point vertex2;
+	const Point vertex3;
+
+	const bool rightSide;
+};
+
+#endif /* TRIANGLE_H */
