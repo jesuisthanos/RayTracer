66f51ec41afb53d23c08a9478dd6036e8e73819f
Hồ Sỹ Thành <hst14@live.com>
2020-01-15 11:49:38 +0100

    circle fixed

--- circle.cpp
+++ circle.cpp
@@ -17,10 +17,21 @@
 
 Hit Circle::intersect(const Ray& ray)
 {
+	//cout << ray.D << endl;
+	norm.normalize();
+	//cout << norm << endl;
 	double t = norm.dot(center - ray.O) / norm.dot(ray.D);
 	if (t > 0) {
-		if (ray.D.dot(norm) < 0) return Hit(t, norm);
-		else return Hit(t, -norm);
+		Point P = ray.O + t * ray.D;
+		//cout << P.x << " " << P.y << " " << P.z << endl;
+		double len = (center - P).length();
+		//cout << len << " " << radius << endl;
+		if ((center - P).length() <= radius) {
+			if (ray.D.dot(norm) < 0) return Hit(t, norm);
+					else return Hit(t, -norm);
+			return Hit(t, norm);
+		}
+		
 	}
 	else {
 		return Hit::NO_HIT();
